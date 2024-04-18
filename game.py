d7d900061a3b0f90aa712ff18b8271bfb744f46f
damianayala <damian.villarreal-ayala@menloschool.org>
2024-04-18 14:53:23 -0700

    Game file created

--- game.py
+++ game.py
@@ -1,5 +1,8 @@
+# This code was created by Damian Villarreal-Ayala on April 18 2024
+#Imports
 import mediapipe as mp
 import cv2
 import numpy as np
 from mediapipe.framework.formats import landmark_pb2
 import time
+
