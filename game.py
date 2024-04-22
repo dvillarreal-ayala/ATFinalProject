1f0e1dc7edfd906f29c8cbd9d355975bddcce7ae
damianayala <damian.villarreal-ayala@menloschool.org>
2024-04-22 13:13:17 -0700

    added game class and main but nonfunctional(error)

--- game.py
+++ game.py
@@ -6,3 +6,62 @@ import numpy as np
 from mediapipe.framework.formats import landmark_pb2
 import time
 
+# Library Constants (from FingerTracking)
+BaseOptions = mp.tasks.BaseOptions
+HandLandmarker = mp.tasks.vision.HandLandmarker
+HandLandmarkPoints = mp.solutions.hands.HandLandmark
+HandLandmarkerOptions = mp.tasks.vision.HandLandmarkerOptions
+VisionRunningMode = mp.tasks.vision.RunningMode
+DrawingUtil = mp.solutions.drawing_utils
+
+class Game:
+    def __init__(self):
+        # Load game elements
+        self.start_time = time.time()
+
+        # Create the hand detector
+        base_options = BaseOptions(model_asset_path='data/hand_landmarker.task')
+        options = HandLandmarkerOptions(base_options=base_options,
+                                                num_hands=2)
+        self.detector = HandLandmarker.create_from_options(options)
+
+        self.video = cv2.VideoCapture(0)
+
+    def run(self):
+        # Begin writing code
+        while self.video.isOpened():
+            # Get the current frame
+            frame = self.video.read()[1]
+
+            # Convert it to an RGB image
+            image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+            # The image comes in mirrored - flip it
+            image = cv2.flip(image, 1)
+
+            # Keep track of time
+            start_time = time.time()
+
+            # Convert the image to a readable format and find the hands
+            to_detect = mp.Image(image_format=mp.ImageFormat.SRGB, data=image)
+            results = self.detector.detect(to_detect)
+
+            # Draw the hand landmarks
+            self.draw_landmarks_on_hand(image, results)
+            # self.check_enemy_kill(image, results)
+
+            # Change the color of the frame back
+            image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+            cv2.imshow('Hand Tracking', image)
+
+            # Break the loop if the user presses 'q'
+            if cv2.waitKey(50) & 0xFF == ord('q'):
+                print(self.score)
+                break
+            
+        self.video.release()
+        cv2.destroyAllWindows()
+            
+if __name__ == "__main__":
+    g = Game()
+    g.run()
