ea4b538b0547d3065d0bad2a2edea3d4252b23cf
damianayala <damian.villarreal-ayala@menloschool.org>
2024-05-09 17:29:56 -0700

    Share Fair Code

--- game.py
+++ game.py
@@ -6,7 +6,7 @@ import cv2
 import numpy as np
 from mediapipe.framework.formats import landmark_pb2
 import time
-# from playsound import playsound
+import random
 
 RED = (255, 0, 0)
 GREEN = (0, 255, 0)
@@ -20,11 +20,73 @@ HandLandmarkerOptions = mp.tasks.vision.HandLandmarkerOptions
 VisionRunningMode = mp.tasks.vision.RunningMode
 DrawingUtil = mp.solutions.drawing_utils
 
+class Enemy:
+    """
+    A class to represent a random circle
+    enemy. It spawns randomly within 
+    the given bounds.
+    """
+    def __init__(self):
+        self.screen_width = 1250
+        self.screen_height = 400
+        self.intercepted = False
+        self.respawn()
+    
+    def respawn(self):
+        """
+        Selects a random location on the screen to respawn
+        """
+        self.x = self.screen_width - 50
+        self.y = random.randint(50, self.screen_height)
+    
+    def draw(self, image):
+        """
+        Enemy is drawn as a circle onto the image
+
+        Args:
+            image (Image): The image to draw the enemy onto
+        """
+        cv2.circle(image, (self.x, self.y), 25, RED, 5)
+
+    def check_interception(self, start_point, end_point):
+        """
+        Check if the enemy is intercepted by the line segment formed by start_point and end_point.
+
+        Args:
+            start_point (tuple): The starting point of the line segment
+            end_point (tuple): The ending point of the line segment
+
+        Returns:
+            bool: True if intercepted, False otherwise
+        """
+        # Calculate the vector representing the aiming line
+        aiming_vector = np.array([end_point[0] - start_point[0], end_point[1] - start_point[1]])
+
+        # Calculate the vector representing the line segment from enemy's center to the starting point of the aiming line
+        enemy_to_start = np.array([start_point[0] - self.x, start_point[1] - self.y])
+
+        # Calculate the dot product of aiming_vector and enemy_to_start
+        dot_product = np.dot(aiming_vector, enemy_to_start)
+
+        # Calculate the length of the aiming vector
+        aiming_length = np.linalg.norm(aiming_vector)
+
+        # Calculate the perpendicular distance from the enemy's center to the aiming line
+        perpendicular_distance = abs(dot_product) / aiming_length
+
+        # If the perpendicular distance is less than the radius of the enemy circle, they intercept
+        if perpendicular_distance <= 25:  # Assuming the radius of the enemy circle is 25
+            return True
+        else:
+            return False
+
 class Game:
     def __init__(self):
         # Load game elements
         self.start_time = time.time()
 
+        # Initialize enemies
+        self.enemy = Enemy()
         # Create the hand detector
         base_options = BaseOptions(model_asset_path='data/hand_landmarker.task')
         options = HandLandmarkerOptions(base_options=base_options,
@@ -79,13 +141,13 @@ class Game:
                 # Draw the circle around the index finger
                 cv2.circle(image, (pixelCoordindex[0], pixelCoordindex[1]), 25, GREEN, 5)
             if pixelCoordmiddle:
-                # Draw the circle around the index finger
+                # Draw the circle around the middle finger
                 cv2.circle(image, (pixelCoordmiddle[0], pixelCoordmiddle[1]), 25, GREEN, 5)        
             if pixelCoordthumb:
-                # Draw the circle around the index finger
+                # Draw the circle around the thumb
                 cv2.circle(image, (pixelCoordthumb[0], pixelCoordthumb[1]), 25, RED, 5)
             if pixelCoordring:
-                # Draw the circle around the index finger
+                # Draw the circle around the ring finger
                 cv2.circle(image, (pixelCoordring[0], pixelCoordring[1]), 25, RED, 5)
 
     # fy_axis_detection was originally written by Damian but edited using ChatGPT when 
@@ -107,29 +169,40 @@ class Game:
             ring = hand_landmarks[HandLandmarkPoints.RING_FINGER_TIP.value]
             thumb = hand_landmarks[HandLandmarkPoints.THUMB_TIP.value]
 
-            pixelCoordindex = DrawingUtil._normalized_to_pixel_coordinates(index.x, index.y, imageWidth, imageHeight)
+            pixelCoordring = DrawingUtil._normalized_to_pixel_coordinates(ring.x, ring.y, imageWidth, imageHeight)
             pixelCoordmiddle = DrawingUtil._normalized_to_pixel_coordinates(middle.x, middle.y, imageWidth, imageHeight)
             pixelCoord_palmpoint = DrawingUtil._normalized_to_pixel_coordinates(palm.x, palm.y, imageWidth, imageHeight)
             pixelCoordthumb = DrawingUtil._normalized_to_pixel_coordinates(thumb.x, thumb.y, imageWidth, imageHeight)
 
+            # If the index finger has a lower y-axis value, it is higher on the screen than the middle finger
+            # If it's higher on the screen, the user is likely aiming
             if index.y < middle.y:
                 starting_point = (pixelCoordmiddle[0], pixelCoordmiddle[1])
-                #palm_point = (pixelCoord_palmpoint[0], pixelCoord_palmpoint[1])
-                #print(starting_point)
-                #cv2.circle(image, starting_point, 25, BLUE, 5)
-
+                # The slope between the middle finger and middle finger mcp is used to aim the Line
                 slope = self.slope(pixelCoord_palmpoint[0], pixelCoord_palmpoint[1], pixelCoordmiddle[0], pixelCoordmiddle[1])
-                # print(slope)
                 
+                # The line will always end at the end of the screen, so we only need to change the y-axis
                 end_point_y = int(starting_point[1] + (imageWidth - starting_point[0]) * slope)
                 end_point = (imageWidth, end_point_y)
-                # Draw line from starting_point to endpoint
+                # Draw line from starting_point to end_point
                 cv2.line(image, starting_point, end_point, BLUE, 5)
                 # Gun will shoot when thumb tip and ring finger tip overlap
-                if ring.x > (thumb.x - 25) and ring.x < (thumb.x + 25):
-                    if ring.y > (thumb.y - 25) and ring.y < (thumb.y + 25):
-                        print("Overlapping fingers")
+                if self.check_shooting(pixelCoordthumb,pixelCoordring) == True:
+                        cv2.line(image, starting_point, end_point, RED, 5)
+                        return [starting_point, end_point]
+
                         
+    def check_shooting(self, pixelCoordthumb, pixelCoordring):
+        if pixelCoordthumb and pixelCoordring:
+                    thumb_center = (pixelCoordthumb[0], pixelCoordthumb[1])
+                    ring_center = (pixelCoordring[0], pixelCoordring[1])
+                    distance = np.linalg.norm(np.array(thumb_center) - np.array(ring_center))
+                    if distance < 25: 
+                        #print("Overlapping fingers")
+                        return True
+                    else:
+                        return False
+
 
     def slope(self, x1, y1, x2, y2):
         # Ensure that the x-values won't be zero when divided
@@ -159,19 +232,24 @@ class Game:
             to_detect = mp.Image(image_format=mp.ImageFormat.SRGB, data=image)
             results = self.detector.detect(to_detect)
 
-            # Draw the hand landmarks
-            # self.draw_landmarks_on_hand(image, results)
-
-            # Draw circle around fingers when extended
+            # Draw circle around fingers 
             self.finger_detection(image, results, start_time)
 
-            # Checks to see if HandLandmarks 8, 7, 6 are roughly in a line
-            # and if 8 has a higher y-value than 12, which indicates that the gun shape is made
-            self.fy_axis_detection(image, results)
+            # Draw the enemy/target
+            self.enemy.draw(image)
+            
+            # Checks to see if the user is aiming with their hand
+            aiming_line_intercepts = self.fy_axis_detection(image, results)
+
+            # Check if the aiming line intercepts the enemy
+            if aiming_line_intercepts:
+                interception = self.enemy.check_interception(aiming_line_intercepts[0], aiming_line_intercepts[1])
+                if interception:
+                    self.enemy.respawn()
 
             # Change the color of the frame back
             image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-            cv2.imshow('Hand Tracking', image)
+            cv2.imshow('First Person Shooter', image)
 
             # Break the loop if the user presses 'q'
             if cv2.waitKey(50) & 0xFF == ord('q'):
